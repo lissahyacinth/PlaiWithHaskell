74f291c66402d3c979a0c87d1e979d60434b7b5d
Lissa Hyacinth <lissahyacinth@gmail.com>
2026-03-25 13:27:07 +0000

    Add switch statements, classes, and tests for those items

--- EvenSmallerInterpreter/src/Interp.hs
+++ EvenSmallerInterpreter/src/Interp.hs
@@ -10,136 +10,183 @@ emptyTEnv :: TypeEnvironment
 emptyTEnv = Map.empty
 
 type Symbol = String
+
 type Environment = Map Symbol Value
+
 type TypeEnvironment = Map Symbol Type
 
 data BinOp
-    = Plus
-    | Concat
-    | Division
-    deriving (Show, Eq)
+  = Plus
+  | Concat
+  | Division
+  | Eq
+  deriving (Show, Eq)
 
 data Type
-    = NumT
-    | StrT
-    | VarT
-    | BoolT
-    | FunctionT Type Type -- Input Type -> Output Type
-    deriving (Show, Eq)
+  = NumT
+  | StrT
+  | VarT
+  | BoolT
+  | FunctionT Type Type -- Input Type -> Output Type
+  deriving (Show, Eq)
 
 data Value
-    = NumV Float
-    | StrV String
-    | BoolV Bool
-    | FunctionV Symbol Expr Environment -- Symbol, Body, Environment
-    deriving (Show, Eq)
+  = NumV Float
+  | StrV String
+  | BoolV Bool
+  | FunctionV Symbol Expr Environment -- Symbol, Body, Environment
+  deriving (Show, Eq)
 
 data Expr
-    = BinE BinOp Expr Expr
-    | NumE Float
-    | StrE String
-    | VarE Symbol
-    | LamE Symbol Type Expr
-    | CondE Expr Expr Expr -- Conditional -> Bool IfTrue IfFalse
-    | AppE Expr Expr
-    | Let1E Symbol Type Expr Expr -- Desugared out
-    deriving (Show, Eq)
+  = BinE BinOp Expr Expr
+  | BoolE Bool
+  | NumE Float
+  | StrE String
+  | VarE Symbol
+  | LamE Symbol Type Expr
+  | CondE Expr Expr Expr -- Conditional -> Bool IfTrue IfFalse
+  | AppE Expr Expr -- Function, Argument (Calculate Function with Argument in Context)
+  | Let1E Symbol Type Expr Expr -- Desugared out
+  -- Class Name is constructed of Functions
+  | DefineE Symbol [(String, Expr)] Expr -- Define X as a List of Expressions
+  | SwitchE [(String, Expr)] Expr
+  | Unreachable
+  deriving (Show, Eq)
 
+-- Desugaring/MacrosmethodName
 desugar :: Expr -> Expr
+desugar Unreachable = Unreachable
+-- e.g let sym: t = e1 in e2
 desugar (Let1E sym t e1 e2) = AppE (LamE sym t e2) e1
 desugar (BinE op l r) = BinE op (desugar l) (desugar r)
-desugar (AppE f a ) = AppE (desugar f) (desugar a)
+desugar (AppE f a) = AppE (desugar f) (desugar a)
 desugar (LamE s t e) = LamE s t (desugar e)
 desugar (CondE cond pos neg) = CondE (desugar cond) (desugar pos) (desugar neg)
 desugar (NumE f) = NumE f
 desugar (StrE s) = StrE s
 desugar (VarE v) = VarE v
+desugar (BoolE v) = BoolE v
+desugar (DefineE className functionList env) =
+  -- Need an AppE to get something into the environment.
+  -- But AppE only takes  (LamE -> FunctionV)
+  -- Let ClassName = DispatchFunction in evaluation
+  desugar
+    ( Let1E
+        className
+        StrT
+        ( desugar
+            ( LamE
+                "methodName"
+                StrT
+                (desugar (SwitchE functionList (VarE "methodName")))
+            )
+        )
+        env
+    )
+-- Peel off the top item, add it to conditional with the next part as the remaining list
+desugar (SwitchE conditionList conditionArg) =
+  case conditionList of
+    [(methodName, func)] ->
+      desugar
+        ( CondE
+            (AppE (LamE "method" StrT (BinE Eq (StrE methodName) (VarE "method"))) conditionArg)
+            func
+            Unreachable
+        )
+    ((methodName, func) : remainingFn) ->
+      desugar
+        ( CondE
+            (AppE (LamE "method" StrT (BinE Eq (StrE methodName) (VarE "method"))) conditionArg)
+            func
+            (SwitchE remainingFn conditionArg)
+        )
+    [] -> error "Unknown case"
 
 calc :: Expr -> Environment -> Value
+calc (SwitchE _ _) _ = error "Should be desugared out"
+calc (DefineE {}) _ = error "Should be desugared out"
+calc Unreachable _ = error "Should be unreachable"
 calc (NumE n) _ = NumV n
 calc (StrE s) _ = StrV s
+calc (BoolE e) _ = BoolV e
 calc (CondE cond pos neg) env =
-    case calc cond env of
-        BoolV True -> calc pos env
-        BoolV False -> calc neg env
-        _ -> error "Type Error, expected BoolV"
+  case calc cond env of
+    BoolV True -> calc pos env
+    BoolV False -> calc neg env
+    FunctionV {} -> error "Received FunctionV, expected BoolV"
+    _ -> error "Type Error, expected BoolV"
 calc (VarE sym) env =
-    case Map.lookup sym env of
-        Just v -> v
-        Nothing -> error ("Unbound identifier: " ++ sym)
-
+  case Map.lookup sym env of
+    Just v -> v
+    Nothing -> error ("Unbound identifier: " ++ sym)
 calc (LamE sym _ expr) env = FunctionV sym expr env
-
 calc (AppE fun arg) env =
-    case calc fun env of
-        FunctionV sym fn_body fnEnv -> calc fn_body (Map.insert sym (calc arg env) fnEnv)
-        _ -> error "Type error, expected FunctionV"
-
-
-calc (BinE Division (NumE n) (NumE m)) _ = NumV(n / m)
+  case calc fun env of
+    FunctionV sym fn_body fnEnv -> calc fn_body (Map.insert sym (calc arg env) fnEnv)
+    _ -> error "Type error, expected FunctionV"
+-- Calculation/Evaluation
+calc (BinE Eq lhs rhs) env =
+  case (calc lhs env, calc rhs env) of
+    (BoolV a, BoolV b) -> BoolV (a == b)
+    (StrV a, StrV b) -> BoolV (a == b)
+    (NumV a, NumV b) -> BoolV (a == b)
+    _ -> error "Type Error"
+calc (BinE Division (NumE n) (NumE m)) _ = NumV (n / m)
 calc (BinE Division lhs rhs) env =
-    case (calc lhs env, calc rhs env) of
-        (NumV a, NumV b) -> NumV (a / b)
-        _ -> error "Type error"
-
-calc (BinE Plus (NumE n) (NumE m)) _ = NumV(n + m)
+  case (calc lhs env, calc rhs env) of
+    (NumV a, NumV b) -> NumV (a / b)
+    _ -> error "Type error"
+calc (BinE Plus (NumE n) (NumE m)) _ = NumV (n + m)
 calc (BinE Plus lhs rhs) env =
-    case (calc lhs env, calc rhs env) of
-        (NumV a, NumV b) -> NumV (a + b)
-        _ -> error "Type error"
-
-
-calc (BinE Concat (StrE n) (StrE m)) _ = StrV(n ++ m)
+  case (calc lhs env, calc rhs env) of
+    (NumV a, NumV b) -> NumV (a + b)
+    _ -> error "Type error"
+calc (BinE Concat (StrE n) (StrE m)) _ = StrV (n ++ m)
 calc (BinE Concat lhs rhs) env =
-    case (calc lhs env, calc rhs env) of
-        (StrV a, StrV b) -> StrV (a ++ b)
-        _ -> error "Type error"
-
+  case (calc lhs env, calc rhs env) of
+    (StrV a, StrV b) -> StrV (a ++ b)
+    _ -> error "Type error"
 calc (Let1E {}) _ = error "Should be desugared out"
 
+-- Type Checking
 tc :: Expr -> TypeEnvironment -> Type
 tc (BinE Plus lhs rhs) env =
-    case (tc lhs env, tc rhs env) of
-        (NumT, NumT) -> NumT
-        _ -> error "Type Error - Plus Requires Two Numbers"
-
+  case (tc lhs env, tc rhs env) of
+    (NumT, NumT) -> NumT
+    _ -> error "Type Error - Plus Requires Two Numbers"
 tc (BinE Division lhs rhs) env =
-    case (tc lhs env, tc rhs env) of
-        (NumT, NumT) -> NumT
-        _ -> error "Type Error - Division Requires Two Numbers"
-
+  case (tc lhs env, tc rhs env) of
+    (NumT, NumT) -> NumT
+    _ -> error "Type Error - Division Requires Two Numbers"
 tc (BinE Concat lhs rhs) env =
-    case (tc lhs env, tc rhs env) of
-        (StrT, StrT) -> StrT
-        _ -> error "Type Error - Concat Requires Two Strings"
-
+  case (tc lhs env, tc rhs env) of
+    (StrT, StrT) -> StrT
+    _ -> error "Type Error - Concat Requires Two Strings"
 tc (NumE _) _ = NumT
 tc (StrE _) _ = StrT
 tc (VarE sym) env =
-    case Map.lookup sym env of
-        Just v -> v
-        Nothing -> error ("Unbound identifier: " ++ sym)
-
+  case Map.lookup sym env of
+    Just v -> v
+    Nothing -> error ("Unbound identifier: " ++ sym)
 tc (LamE sym t1 expr) env =
-    FunctionT t1 (tc expr (Map.insert sym t1 env))
-
+  FunctionT t1 (tc expr (Map.insert sym t1 env))
 tc (AppE fun arg) env =
-    -- |- C: U, |- A: U, |- B: D
-    -- -------------------------
-    -- AppE (LamE A B) C : D
-    case tc fun env of
-        FunctionT inType outType ->
-            if tc arg env == inType then outType
-            else error "Argument didn't match Argument Type"
-        _ -> error "Expect function type within lambda"
-
+  -- \|- C: U, |- A: U, |- B: D
+  -- -------------------------
+  -- AppE (LamE A B) C : D
+  case tc fun env of
+    FunctionT inType outType ->
+      if tc arg env == inType
+        then outType
+        else error "Argument didn't match Argument Type"
+    _ -> error "Expect function type within lambda"
 tc (CondE cond pos neg) env =
-    case tc cond env of
-        BoolT | posT == negT -> posT
-              |  otherwise -> error "Expected positive and negative branches to share type"
-        _ -> error "Expect Bool from conditional"
-    where
-        posT = tc pos env
-        negT = tc neg env
-
+  case tc cond env of
+    BoolT
+      | posT == negT -> posT
+      | otherwise -> error "Expected positive and negative branches to share type"
+    _ -> error "Expect Bool from conditional"
+  where
+    posT = tc pos env
+    negT = tc neg env
 tc (Let1E {}) _ = error "Should be desugared out"
--- EvenSmallerInterpreter/test/Main.hs
+++ EvenSmallerInterpreter/test/Main.hs
@@ -1,91 +1,142 @@
-import Test.Hspec
 import Control.Exception (evaluate)
-import Interp (Expr(..), Value(..), BinOp(..), Type(..), calc, tc, desugar, emptyEnv, emptyTEnv)
+import Interp (BinOp (..), Expr (..), Type (..), Value (..), calc, desugar, emptyEnv, emptyTEnv, tc)
+import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-    describe "addition" $ do
-        it "adds two numbers" $
-            calc (BinE Plus (NumE 1) (NumE 2)) emptyEnv `shouldBe` NumV 3
-        it "concats two strings" $
-            calc (BinE Concat (StrE "1") (StrE "2")) emptyEnv `shouldBe` StrV "12"
-    describe "division" $ do
-        it "divide two numbers" $
-            calc (BinE Division (NumE 1) (NumE 2)) emptyEnv `shouldBe` NumV 0.5
+  describe "addition" $ do
+    it "adds two numbers" $
+      calc (BinE Plus (NumE 1) (NumE 2)) emptyEnv `shouldBe` NumV 3
+    it "concats two strings" $
+      calc (BinE Concat (StrE "1") (StrE "2")) emptyEnv `shouldBe` StrV "12"
+  describe "division" $ do
+    it "divide two numbers" $
+      calc (BinE Division (NumE 1) (NumE 2)) emptyEnv `shouldBe` NumV 0.5
 
-    describe "lambdas and application" $ do
-        it "identity function returns its argument" $
-            -- (\x : Num -> x) 5
-            calc (AppE (LamE "x" NumT (VarE "x")) (NumE 5)) emptyEnv `shouldBe` NumV 5
-        it "constant function ignores second argument" $
-            -- (\x : Num -> (\y : Num -> x)) 7 99
-            calc (AppE (AppE (LamE "x" NumT (LamE "y" NumT (VarE "x"))) (NumE 7)) (NumE 99)) emptyEnv `shouldBe` NumV 7
-        it "lambda with plus in body" $
-            -- (\x : Num -> x + 1) 4
-            calc (AppE (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) (NumE 4)) emptyEnv `shouldBe` NumV 5
-        it "nested application with plus" $
-            -- (\f : Fn -> f 3) (\x : Num -> x + 10)
-            calc (AppE (LamE "f" (FunctionT NumT NumT) (AppE (VarE "f") (NumE 3))) (LamE "x" NumT (BinE Plus (VarE "x") (NumE 10)))) emptyEnv `shouldBe` NumV 13
-        it "applying non-function throws" $
-            evaluate (calc (AppE (NumE 1) (NumE 2)) emptyEnv) `shouldThrow` anyException
-        it "unbound variable throws" $
-            evaluate (calc (VarE "x") emptyEnv) `shouldThrow` anyException
-        it "lexical scoping - closure captures definition environment" $
-            -- let x = 1 in (let f = (\y -> x + y) in (let x = 99 in f 0))
-            -- should return 1, not 99
-            let expr = AppE (LamE "x" NumT
-                        (AppE (LamE "f" (FunctionT NumT NumT)
-                            (AppE (LamE "x" NumT
-                                (AppE (VarE "f") (NumE 0)))
-                            (NumE 99)))
-                        (LamE "y" NumT (BinE Plus (VarE "x") (VarE "y")))))
-                      (NumE 1)
-            in calc expr emptyEnv `shouldBe` NumV 1
+  describe "lambdas and application" $ do
+    it "identity function returns its argument" $
+      -- (\x : Num -> x) 5
+      calc (AppE (LamE "x" NumT (VarE "x")) (NumE 5)) emptyEnv `shouldBe` NumV 5
+    it "constant function ignores second argument" $
+      -- (\x : Num -> (\y : Num -> x)) 7 99
+      calc (AppE (AppE (LamE "x" NumT (LamE "y" NumT (VarE "x"))) (NumE 7)) (NumE 99)) emptyEnv `shouldBe` NumV 7
+    it "lambda with plus in body" $
+      -- (\x : Num -> x + 1) 4
+      calc (AppE (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) (NumE 4)) emptyEnv `shouldBe` NumV 5
+    it "nested application with plus" $
+      -- (\f : Fn -> f 3) (\x : Num -> x + 10)
+      calc (AppE (LamE "f" (FunctionT NumT NumT) (AppE (VarE "f") (NumE 3))) (LamE "x" NumT (BinE Plus (VarE "x") (NumE 10)))) emptyEnv `shouldBe` NumV 13
+    it "applying non-function throws" $
+      evaluate (calc (AppE (NumE 1) (NumE 2)) emptyEnv) `shouldThrow` anyException
+    it "unbound variable throws" $
+      evaluate (calc (VarE "x") emptyEnv) `shouldThrow` anyException
+    it "lexical scoping - closure captures definition environment" $
+      -- let x = 1 in (let f = (\y -> x + y) in (let x = 99 in f 0))
+      -- should return 1, not 99
+      let expr =
+            AppE
+              ( LamE
+                  "x"
+                  NumT
+                  ( AppE
+                      ( LamE
+                          "f"
+                          (FunctionT NumT NumT)
+                          ( AppE
+                              ( LamE
+                                  "x"
+                                  NumT
+                                  (AppE (VarE "f") (NumE 0))
+                              )
+                              (NumE 99)
+                          )
+                      )
+                      (LamE "y" NumT (BinE Plus (VarE "x") (VarE "y")))
+                  )
+              )
+              (NumE 1)
+       in calc expr emptyEnv `shouldBe` NumV 1
 
-    describe "desugaring Let1E" $ do
-        it "let x : Num = 5 in x" $
-            calc (desugar (Let1E "x" NumT (NumE 5) (VarE "x"))) emptyEnv `shouldBe` NumV 5
-        it "let x : Num = 2 in x + 3" $
-            calc (desugar (Let1E "x" NumT (NumE 2) (BinE Plus (VarE "x") (NumE 3)))) emptyEnv `shouldBe` NumV 5
-        it "nested let - let x : Num = 1 in let y : Num = 2 in x + y" $
-            calc (desugar (Let1E "x" NumT (NumE 1) (desugar (Let1E "y" NumT (NumE 2) (BinE Plus (VarE "x") (VarE "y")))))) emptyEnv `shouldBe` NumV 3
-        it "let shadows outer binding" $
-            let expr = desugar (Let1E "x" NumT (NumE 10)
-                        (desugar (Let1E "x" NumT (NumE 20) (VarE "x"))))
-            in calc expr emptyEnv `shouldBe` NumV 20
+  describe "Calculating Switch" $ do
+    it "desugars and calculates a conditionList" $
+      calc
+        ( desugar
+            ( SwitchE
+                [ ("method1", (Let1E "x" NumT (NumE 5) (VarE "x"))),
+                  ("method2", (Let1E "x" NumT (NumE 6) (VarE "x")))
+                ]
+                (StrE "method1")
+            )
+        )
+        emptyEnv
+        `shouldBe` NumV 5
+    it "desugars a classE" $
+      calc
+        ( desugar
+            ( DefineE
+                "myClass"
+                [ ("method1", (Let1E "x" NumT (NumE 5) (VarE "x"))),
+                  ("method2", (Let1E "x" NumT (NumE 6) (VarE "x")))
+                ]
+                ( AppE
+                    (VarE "myClass")
+                    (StrE "method1")
+                )
+            )
+        )
+        emptyEnv
+        `shouldBe` NumV 5
 
-    describe "type checking" $ do
-        it "addition" $
-            tc (BinE Plus (NumE 1) (NumE 2)) emptyTEnv `shouldBe` NumT
-        it "addition negative" $
-            evaluate (tc (BinE Plus (StrE "1") (NumE 2)) emptyTEnv) `shouldThrow` anyException
-        it "division" $
-            tc (BinE Division (NumE 1) (NumE 2)) emptyTEnv `shouldBe` NumT
-        it "division negative" $
-            evaluate (tc (BinE Division (StrE "1") (NumE 2)) emptyTEnv) `shouldThrow` anyException
-        it "concats" $
-            tc (BinE Concat (StrE "1") (StrE "2")) emptyTEnv `shouldBe` StrT
-        it "concats negative" $
-            evaluate (tc (BinE Concat (NumE 1) (StrE "2")) emptyTEnv) `shouldThrow` anyException
+  describe "desugaring Let1E" $ do
+    it "let x : Num = 5 in x" $
+      calc (desugar (Let1E "x" NumT (NumE 5) (VarE "x"))) emptyEnv `shouldBe` NumV 5
+    it "let x : Num = 2 in x + 3" $
+      calc (desugar (Let1E "x" NumT (NumE 2) (BinE Plus (VarE "x") (NumE 3)))) emptyEnv `shouldBe` NumV 5
+    it "nested let - let x : Num = 1 in let y : Num = 2 in x + y" $
+      calc (desugar (Let1E "x" NumT (NumE 1) (desugar (Let1E "y" NumT (NumE 2) (BinE Plus (VarE "x") (VarE "y")))))) emptyEnv `shouldBe` NumV 3
+    it "let shadows outer binding" $
+      let expr =
+            desugar
+              ( Let1E
+                  "x"
+                  NumT
+                  (NumE 10)
+                  (desugar (Let1E "x" NumT (NumE 20) (VarE "x")))
+              )
+       in calc expr emptyEnv `shouldBe` NumV 20
 
-    describe "type checking lambdas and application" $ do
-        it "identity on number - (\\x : Num -> x) 5" $
-            tc (AppE (LamE "x" NumT (VarE "x")) (NumE 5)) emptyTEnv `shouldBe` NumT
-        it "identity on string - (\\x : Str -> x) \"hi\"" $
-            tc (AppE (LamE "x" StrT (VarE "x")) (StrE "hi")) emptyTEnv `shouldBe` StrT
-        it "lambda with plus - (\\x : Num -> x + 1) 4" $
-            tc (AppE (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) (NumE 4)) emptyTEnv `shouldBe` NumT
-        it "argument type mismatch errors - (\\x : Num -> x + 1) \"hi\"" $
-            evaluate (tc (AppE (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) (StrE "hi")) emptyTEnv) `shouldThrow` anyException
-        it "applying non-function errors" $
-            evaluate (tc (AppE (NumE 1) (NumE 2)) emptyTEnv) `shouldThrow` anyException
-        it "unbound variable errors" $
-            evaluate (tc (VarE "x") emptyTEnv) `shouldThrow` anyException
-        it "lambda type is FunctionT" $
-            tc (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) emptyTEnv `shouldBe` FunctionT NumT NumT
-        it "nested application - (\\f : Num->Num -> f 3) (\\x : Num -> x + 10)" $
-            tc (AppE (LamE "f" (FunctionT NumT NumT) (AppE (VarE "f") (NumE 3))) (LamE "x" NumT (BinE Plus (VarE "x") (NumE 10)))) emptyTEnv `shouldBe` NumT
-        it "desugared let typechecks - let x : Num = 5 in x + 1" $
-            tc (desugar (Let1E "x" NumT (NumE 5) (BinE Plus (VarE "x") (NumE 1)))) emptyTEnv `shouldBe` NumT
-        it "desugared let catches type error - let x : Num = \"hi\" in x + 1" $
-            evaluate (tc (desugar (Let1E "x" NumT (StrE "hi") (BinE Plus (VarE "x") (NumE 1)))) emptyTEnv) `shouldThrow` anyException
+  describe "type checking" $ do
+    it "addition" $
+      tc (BinE Plus (NumE 1) (NumE 2)) emptyTEnv `shouldBe` NumT
+    it "addition negative" $
+      evaluate (tc (BinE Plus (StrE "1") (NumE 2)) emptyTEnv) `shouldThrow` anyException
+    it "division" $
+      tc (BinE Division (NumE 1) (NumE 2)) emptyTEnv `shouldBe` NumT
+    it "division negative" $
+      evaluate (tc (BinE Division (StrE "1") (NumE 2)) emptyTEnv) `shouldThrow` anyException
+    it "concats" $
+      tc (BinE Concat (StrE "1") (StrE "2")) emptyTEnv `shouldBe` StrT
+    it "concats negative" $
+      evaluate (tc (BinE Concat (NumE 1) (StrE "2")) emptyTEnv) `shouldThrow` anyException
+
+  describe "type checking lambdas and application" $ do
+    it "identity on number - (\\x : Num -> x) 5" $
+      tc (AppE (LamE "x" NumT (VarE "x")) (NumE 5)) emptyTEnv `shouldBe` NumT
+    it "identity on string - (\\x : Str -> x) \"hi\"" $
+      tc (AppE (LamE "x" StrT (VarE "x")) (StrE "hi")) emptyTEnv `shouldBe` StrT
+    it "lambda with plus - (\\x : Num -> x + 1) 4" $
+      tc (AppE (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) (NumE 4)) emptyTEnv `shouldBe` NumT
+    it "argument type mismatch errors - (\\x : Num -> x + 1) \"hi\"" $
+      evaluate (tc (AppE (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) (StrE "hi")) emptyTEnv) `shouldThrow` anyException
+    it "applying non-function errors" $
+      evaluate (tc (AppE (NumE 1) (NumE 2)) emptyTEnv) `shouldThrow` anyException
+    it "unbound variable errors" $
+      evaluate (tc (VarE "x") emptyTEnv) `shouldThrow` anyException
+    it "lambda type is FunctionT" $
+      tc (LamE "x" NumT (BinE Plus (VarE "x") (NumE 1))) emptyTEnv `shouldBe` FunctionT NumT NumT
+    it "nested application - (\\f : Num->Num -> f 3) (\\x : Num -> x + 10)" $
+      tc (AppE (LamE "f" (FunctionT NumT NumT) (AppE (VarE "f") (NumE 3))) (LamE "x" NumT (BinE Plus (VarE "x") (NumE 10)))) emptyTEnv `shouldBe` NumT
+    it "desugared let typechecks - let x : Num = 5 in x + 1" $
+      tc (desugar (Let1E "x" NumT (NumE 5) (BinE Plus (VarE "x") (NumE 1)))) emptyTEnv `shouldBe` NumT
+    it "desugared let catches type error - let x : Num = \"hi\" in x + 1" $
+      evaluate (tc (desugar (Let1E "x" NumT (StrE "hi") (BinE Plus (VarE "x") (NumE 1)))) emptyTEnv) `shouldThrow` anyException
